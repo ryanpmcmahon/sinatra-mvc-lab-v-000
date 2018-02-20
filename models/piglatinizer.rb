68dd9293042a2f66b631f144039f2b8064c92212
ryanpmcmahon <ryan@mcmahon.work>
2018-02-19 21:35:46 -0500

    wip

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -7,7 +7,26 @@ class PigLatinizer
   end
 
   def word_checker
-    text.map{|word| word.split(/[aeiouy]/).push('ay').join("")}.join(" ")
+    # split words into individual letters
+    #
+    # loop through letters of words
+    # 
+    # if letter is a consonant, set to placeholder variable, shift, push to end
+    #
+    # if letter is a vowel, jump to end of word
+    #
+    # add 'ay'
+
+    word_arr = text.map{|word| word.split()}
+    word_arr.each do |word|
+      word.each_with_index do |l,i|
+        new_word =
+        if !/[aeiouy]/.match(l)
+          new_word << l
+          word.shift
+        end
+      end
+    end
   end
 
 end
